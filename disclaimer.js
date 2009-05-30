5a4151a91318c9d8614230469fc74820f462a79c
Jean <mogtofu33@278510.no-reply.drupal.org>
2009-05-30 02:07:48 +0000

    #471176 by ctalley5: Added age verification. #471908 by miacbi: Added disclaimer visibility, localization through node, enter url no redirect if empty. #476430 by LAN_DRUPAL_aka_...: Fixed Install.txt. Added some other feature : new jQuery modal plugin, jqModal very light. Added node selection for content and footer of the disclaimer. Added new file disclaimer.admin.inc to handle admin form. Minor correction. Updated INSTALL.txt. Updated disclaimer.pot. Updated fr.pot.

--- disclaimer.js
+++ disclaimer.js
@@ -12,12 +12,33 @@ function ReadCookie(nom) {
   var alen=arg.length;
   var clen=document.cookie.length;
   var i=0;
-  while (i < clen)
-  {
+  while (i < clen) {
     var j=i+alen;
     if (document.cookie.substring(i, j)==arg) return getCookieVal(j);
     i=document.cookie.indexOf(" ",i)+1;
     if (i==0) break;
   }
   return null;
+}
+function CheckAge(cookie, limit, exit_url, modal) {
+  var now=new Date();
+  var date=now.getDate();
+  var month=now.getMonth() + 1;
+  var year=now.getFullYear();
+  var optmonth=$("#edit-disclaimer-age-month option:selected").val();
+  var optday=$("#edit-disclaimer-age-day option:selected").val();
+  var optyear=$("#edit-disclaimer-age-year option:selected").val();
+  var age=year-optyear;
+  if (optmonth>month) {age--;} else {if(optmonth==month && optday>=date) {age--;}}
+  if (optyear==year) {
+    alert(Drupal.t("You must enter the year you were born in."));return false;
+  } else if (age<limit) {
+    alert(Drupal.t("Sorry, you are Under age limit and are prohibited from entering this site!"));
+    location.replace(exit_url);return false;
+  } else {
+    // age limit ok, close modal and set cookie
+    WriteCookie(cookie);
+    if (modal=='nyroModal'){$.nyroModalRemove();}else{$('#disclaimer').jqmHide();}
+    return true;
+  }
 }
