85407bc68d4da0d380d4460bc7dbe09b482ffc0d
Radim Klaska <radim@klaska.net>
2018-01-02 16:11:57 +0100

    Fix coding standards.

--- js/sentry.js
+++ js/sentry.js
@@ -3,52 +3,52 @@
  * Attaches popup and show/hide functionality to disclaimer.
  */
 (function ($) {
-    'use strict';
-    Drupal.behaviors.sentry = {
-        attach: function (context, settings) {
-            // Show/Hide based on Javascript availability.
-            $('.sentryNoScript').hide();
+  'use strict';
+  Drupal.behaviors.sentry = {
+    attach: function (context, settings) {
+      // Show/Hide based on Javascript availability.
+      $('.sentryNoScript').hide();
 
-            // Go trough all sentry block instances on thos page.
-            Object.keys(drupalSettings.sentry.sentry).forEach(function (key) {
-                    // Skip popup in case cookie says user already agreed.
-                    if ($.cookie(key) != 1) {
-                        // User did not agreed yet. Show popup.
-                        $('.block-sentry-block.' + key + ' .sentry__challenge', context).dialog({
-                                closeOnEscape: false,
-                                open: function (event, ui) {
-                                    $(".ui-dialog-titlebar-close", ui.dialog | ui).hide();
-                                },
-                                resizable: false,
-                                height: "auto",
-                                width: "40%",
-                                modal: true,
-                                buttons: {
-                                    "Yes": {
-                                        text: Drupal.t("Yes"),
-                                        click: function () {
-                                            $(this).dialog("close");
-                                            var expire = new Date(new Date().getTime() + parseInt(drupalSettings.sentry.sentry[key].max_age) * 1000);
-                                            $.cookie(key, 1, {expires: expire});
-                                        }
-                                    },
-                                    "No": {
-                                        text: Drupal.t("No"),
-                                        click:
+      // Go trough all sentry block instances on thos page.
+      Object.keys(drupalSettings.sentry.sentry).forEach(function (key) {
+            // Skip popup in case cookie says user already agreed.
+            if ($.cookie(key) != 1) {
+              // User did not agreed yet. Show popup.
+              $('.block-sentry-block.' + key + ' .sentry__challenge', context).dialog({
+                    closeOnEscape: false,
+                    open: function (event, ui) {
+                      $(".ui-dialog-titlebar-close", ui.dialog | ui).hide();
+                    },
+                    resizable: false,
+                    height: "auto",
+                    width: "40%",
+                    modal: true,
+                    buttons: {
+                      "Yes": {
+                        text: Drupal.t("Yes"),
+                        click: function () {
+                          $(this).dialog("close");
+                          var expire = new Date(new Date().getTime() + parseInt(drupalSettings.sentry.sentry[key].max_age) * 1000);
+                          $.cookie(key, 1, {expires: expire});
+                        }
+                      },
+                      "No": {
+                        text: Drupal.t("No"),
+                        click:
 
-                                            function () {
-                                                $(this).dialog("close");
-                                                window.location.href = drupalSettings.sentry.sentry[key].redirect;
-                                            }
-                                    }
-                                }
+                            function () {
+                              $(this).dialog("close");
+                              window.location.href = drupalSettings.sentry.sentry[key].redirect;
                             }
-                        );
+                      }
                     }
-                }
-            );
-        }
+                  }
+              );
+            }
+          }
+      );
     }
-    ;
+  }
+  ;
 })
 (jQuery);
--- src/Plugin/Block/SentryBlock.php
+++ src/Plugin/Block/SentryBlock.php
@@ -3,11 +3,9 @@
 namespace Drupal\sentry\Plugin\Block;
 
 use Drupal\Component\Utility\Html;
-use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
 
 /**
  * Provides a 'SentryBlock' block.
@@ -17,178 +15,174 @@ use Drupal\Core\Url;
  *  admin_label = @Translation("Sentry block"),
  * )
  */
-class SentryBlock extends BlockBase
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function getCacheContexts()
-    {
-        return Cache::mergeContexts(parent::getCacheContexts(), ['cookies:sentry_' . $this->configuration['machine_name'], 'url.path']);
+class SentryBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), [
+      'cookies:sentry_' . $this->configuration['machine_name'],
+      'url.path',
+    ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return $this->configuration['max_age'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'machine_name' => $this->t(''),
+      'challenge' => $this->t(''),
+      'disclaimer' => $this->t(''),
+      'redirect' => $this->t('/'),
+      'max_age' => $this->t('86400'),
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    // $current_user = \Drupal::currentUser();
+    //        $filter_formats = filter_formats($current_user);
+    //        $filter_formats_form = [];
+    //        foreach ($filter_formats as $filter_format) {
+    //            $filter_formats_form[$filter_format->id()] = $filter_format->label();
+    //        }
+    $form['machine_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Machine name'),
+      '#description' => $this->t('Used as ID in cookies.'),
+      '#default_value' => $this->configuration['machine_name'],
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#weight' => '10',
+    ];
+    $form['challenge'] = [
+      // '#type' => 'text_format',
+      //            '#format' => $filter_formats_form,.
+      '#type' => 'textarea',
+      '#title' => $this->t('Challenge'),
+      '#description' => $this->t('The question the user must confirm. "Do you agree?" type of question. "Yes" = User stays on requested page. "No" = User is redirected to <em>Redirect</em> url specified below.'),
+      '#default_value' => $this->configuration['challenge'],
+      '#weight' => '20',
+    ];
+    $form['redirect'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Redirect'),
+      '#description' => $this->t('The URL a rejected user is sent to. eg. /content-for-unconfirmed-users. (relative, absolute, &lt;front&gt;)'),
+      '#default_value' => $this->configuration['redirect'],
+      '#maxlength' => 256,
+      '#size' => 64,
+      '#weight' => '30',
+    ];
+    $form['disclaimer'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Disclaimer'),
+      '#description' => $this->t('The text displayed to the user on a protected page when the user has JS turned off. (No popup wich challenge is available.)'),
+      '#default_value' => $this->configuration['disclaimer'],
+      '#weight' => '40',
+    ];
+    $form['max_age'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Max-age'),
+      '#description' => $this->t('The time in seconds the user is confirmed. Set to 0 for no expiry. (86400 seconds = 24 hours)'),
+      '#default_value' => $this->configuration['max_age'],
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#weight' => '50',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['machine_name'] = $form_state->getValue('machine_name');
+    $this->configuration['challenge'] = $form_state->getValue('challenge');
+    $this->configuration['disclaimer'] = $form_state->getValue('disclaimer');
+    $this->configuration['redirect'] = $form_state->getValue('redirect');
+    $this->configuration['max_age'] = $form_state->getValue('max_age');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockValidate($form, FormStateInterface $form_state) {
+    $url_object = \Drupal::service('path.validator')
+      ->getUrlIfValid($form_state->getValue('redirect'));
+
+    if (!$url_object) {
+      $form_state->setErrorByName('redirect', $this->t('Redirect URL must be valid path.'));
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getCacheMaxAge()
-    {
-        return $this->configuration['max_age'];
+    if (!preg_match('/^[a-zA-Z0-9_]+$/', $form_state->getValue('machine_name'))) {
+      $form_state->setErrorByName('machine_name', $this->t('Machine name must contain only letters, numbers and underscores.'));
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration()
-    {
-        return [
-                'machine_name' => $this->t(''),
-                'challenge' => $this->t(''),
-                'disclaimer' => $this->t(''),
-                'redirect' => $this->t('/'),
-                'max_age' => $this->t('86400'),
-            ] + parent::defaultConfiguration();
+    if (!preg_match('/^[0-9]+$/', $form_state->getValue('max_age'))) {
+      $form_state->setErrorByName('max_age', $this->t('Max-age must be integer.'));
     }
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function blockForm($form, FormStateInterface $form_state)
-    {
-//        $current_user = \Drupal::currentUser();
-//        $filter_formats = filter_formats($current_user);
-//        $filter_formats_form = [];
-//        foreach ($filter_formats as $filter_format) {
-//            $filter_formats_form[$filter_format->id()] = $filter_format->label();
-//        }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Identify block by class with machine name.
+    $build = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [
+          'sentry_' . Html::escape($this->configuration['machine_name']),
+          'sentryNoScript',
+        ],
+      ],
+    ];
+    // Include JS to handle popup and hiding.
+    $build['#attached']['library'][] = 'sentry/sentry';
+    // Pass settings to JS.
+    $build['#attached']['drupalSettings']['sentry']['sentry']['sentry_' . Html::escape($this->configuration['machine_name'])] = [
+      'challenge' => Html::escape($this->configuration['challenge']),
+      'redirect' => $this->configuration['redirect'],
+      'max_age' => Html::escape($this->configuration['max_age']),
+    ];
 
-        $form['machine_name'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Machine name'),
-            '#description' => $this->t('Used as ID in cookies.'),
-            '#default_value' => $this->configuration['machine_name'],
-            '#maxlength' => 64,
-            '#size' => 64,
-            '#weight' => '10',
-        ];
-        $form['challenge'] = [
-//            '#type' => 'text_format',
-//            '#format' => $filter_formats_form,
-            '#type' => 'textarea',
-            '#title' => $this->t('Challenge'),
-            '#description' => $this->t('The question the user must confirm. "Do you agree?" type of question. "Yes" = User stays on requested page. "No" = User is redirected to <em>Redirect</em> url specified below.'),
-            '#default_value' => $this->configuration['challenge'],
-            '#weight' => '20',
-        ];
-        $form['redirect'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Redirect'),
-            '#description' => $this->t('The URL a rejected user is sent to. eg. /content-for-unconfirmed-users. (relative, absolute, &lt;front&gt;)'),
-            '#default_value' => $this->configuration['redirect'],
-            '#maxlength' => 256,
-            '#size' => 64,
-            '#weight' => '30',
-        ];
-        $form['disclaimer'] = [
-            '#type' => 'textarea',
-            '#title' => $this->t('Disclaimer'),
-            '#description' => $this->t('The text displayed to the user on a protected page when the user has JS turned off. (No popup wich challenge is available.)'),
-            '#default_value' => $this->configuration['disclaimer'],
-            '#weight' => '40',
-        ];
-        $form['max_age'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Max-age'),
-            '#description' => $this->t('The time in seconds the user is confirmed. Set to 0 for no expiry. (86400 seconds = 24 hours)'),
-            '#default_value' => $this->configuration['max_age'],
-            '#maxlength' => 64,
-            '#size' => 64,
-            '#weight' => '50',
-        ];
+    // Render disclaimer.
+    $build['sentry_block_disclaimer'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [
+          'sentry__disclaimer',
+        ],
+      ],
+      '#markup' => Html::escape($this->configuration['disclaimer']),
+    ];
 
-        return $form;
-    }
+    // Render popup HTML.
+    $build['sentry_block_challenge'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [
+          'sentry__challenge',
+          'hidden',
+        ],
+        'title' => [
+          Html::escape($this->label()),
+        ],
+      ],
+      '#markup' => Html::escape($this->configuration['challenge']),
+    ];
 
-    /**
-     * {@inheritdoc}
-     */
-    public function blockSubmit($form, FormStateInterface $form_state)
-    {
-        $this->configuration['machine_name'] = $form_state->getValue('machine_name');
-        $this->configuration['challenge'] = $form_state->getValue('challenge');
-        $this->configuration['disclaimer'] = $form_state->getValue('disclaimer');
-        $this->configuration['redirect'] = $form_state->getValue('redirect');
-        $this->configuration['max_age'] = $form_state->getValue('max_age');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function blockValidate($form, FormStateInterface $form_state)
-    {
-        $url_object = \Drupal::service('path.validator')->getUrlIfValid($form_state->getValue('redirect'));
-
-        if (!$url_object) {
-            $form_state->setErrorByName('redirect', $this->t('Redirect URL must be valid path.'));
-        }
-        if (!preg_match('/^[a-zA-Z0-9_]+$/', $form_state->getValue('machine_name'))) {
-            $form_state->setErrorByName('machine_name', $this->t('Machine name must contain only letters, numbers and underscores.'));
-        }
-        if (!preg_match('/^[0-9]+$/', $form_state->getValue('max_age'))) {
-            $form_state->setErrorByName('max_age', $this->t('Max-age must be integer.'));
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function build()
-    {
-        // Identify block by class with machine name.
-        $build = [
-            '#type' => 'container',
-            '#attributes' => [
-                'class' => [
-                    'sentry_' . Html::escape($this->configuration['machine_name']),
-                    'sentryNoScript',
-                ],
-            ],
-        ];
-        // Include JS to handle popup and hiding.
-        $build['#attached']['library'][] = 'sentry/sentry';
-        // Pass settings to JS.
-        $build['#attached']['drupalSettings']['sentry']['sentry']['sentry_' . Html::escape($this->configuration['machine_name'])] = [
-            'challenge' => Html::escape($this->configuration['challenge']),
-            'redirect' => $this->configuration['redirect'],
-            'max_age' => Html::escape($this->configuration['max_age']),
-        ];
-
-        // Render disclaimer.
-        $build['sentry_block_disclaimer'] = [
-            '#type' => 'container',
-            '#attributes' => [
-                'class' => [
-                    'sentry__disclaimer',
-                ],
-            ],
-            '#markup' => Html::escape($this->configuration['disclaimer']),
-        ];
-
-        // Render popup HTML.
-        $build['sentry_block_challenge'] = [
-            '#type' => 'container',
-            '#attributes' => [
-                'class' => [
-                    'sentry__challenge',
-                    'hidden',
-                ],
-                'title' => [
-                    Html::escape($this->label()),
-                ],
-            ],
-            '#markup' => Html::escape($this->configuration['challenge']),
-        ];
-
-        return $build;
-    }
+    return $build;
+  }
 
 }
